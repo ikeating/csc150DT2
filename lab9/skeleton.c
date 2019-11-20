6e4a1e468d18d45b3df92e18540a227e1e908172
Isaiah Keating <44143399+ikeating@users.noreply.github.com>
2019-11-19 19:49:24 -0600

    Add files via upload

---
+++ lab9/skeleton.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+   /*int x = 0;
+   x++;
+   ++x;
+   x+=1;
+   //+x;
+   printf("%i\n", x);
+
+   int total = 0;
+   int item = 5;
+
+   total += item;
+   //total == item;
+   //total = item+1;
+   //total != item;
+
+   printf("%i\n", total);
+
+   //int c = 1.1;
+   int c = -3;
+
+   int a = 5;
+   int b = 0;
+   int c = a/b;
+
+   int i = 5;
+   while (i > 4)
+   {
+   	   printf("hello loop %i \n", i);
+   	   i++;
+   }*/
+
+   int x =2;
+   int y =10;
+
+   printf("%i\n", x/y);
+
+   return(0);
+}
